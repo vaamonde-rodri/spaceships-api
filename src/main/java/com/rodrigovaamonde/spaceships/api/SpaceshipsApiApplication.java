34888ffcbe8d090f94896bf9b3d421bac436c03c
Rodrigo Vaamonde <vaamonde.rodri@gmail.com>
2024-11-03 20:56:43 +0100

    first commit

---
+++ src/main/java/com/rodrigovaamonde/spaceships/api/SpaceshipsApiApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigovaamonde.spaceships.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceshipsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaceshipsApiApplication.class, args);
+    }
+
+}
