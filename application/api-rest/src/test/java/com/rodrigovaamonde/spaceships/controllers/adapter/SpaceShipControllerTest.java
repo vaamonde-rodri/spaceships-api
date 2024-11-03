52d0b00dd77fbad6cf76644f98c64648b756e243
Rodrigo Vaamonde <vaamonde.rodri@gmail.com>
2024-11-04 00:38:03 +0100

    created tests

---
+++ application/api-rest/src/test/java/com/rodrigovaamonde/spaceships/controllers/adapter/SpaceShipControllerTest.java
@@ -0,0 +1,33 @@
+package com.rodrigovaamonde.spaceships.controllers.adapter;
+
+import com.rodrigovaamonde.spaceships.controllers.dto.SpaceShipDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SpaceShipControllerTest {
+
+  private SpaceShipController spaceShipController;
+
+  @BeforeEach
+  public void setUp() {
+    spaceShipController = new SpaceShipController();
+  }
+
+  @Test
+  public void createSpaceShipReturnsResponseEntity() {
+    SpaceShipDTO spaceShipDTO = SpaceShipDTO.builder().build();
+    ResponseEntity<SpaceShipDTO> response = spaceShipController.createSpaceShip(spaceShipDTO);
+    assertEquals(ResponseEntity.ok().build().getStatusCode(), response.getStatusCode());
+  }
+
+  @Test
+  void createSpaceShipWithNullDTO() {
+    ResponseEntity<SpaceShipDTO> response = spaceShipController.createSpaceShip(null);
+    assertEquals(ResponseEntity.badRequest().build().getStatusCode(), response.getStatusCode());
+  }
+}
